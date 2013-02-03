0a22b66d3c2c4e9c32a155dc1b78db34a72c3d84
Armael <armael.gueneau@ens-lyon.fr>
2013-02-03 22:32:15 +0100

    Module Pretty : fonctions permettant d'afficher des structures de données (pour l'instant, des listes).
    Utile pour visualiser l'état du programme et pour debugger.

---
+++ pretty.ml
@@ -0,0 +1,20 @@
+(* Some pretty-printing utility functions.
+
+   A cleaner solution would be to use sexplib from Janestreet's Core
+   library, but I don't want here to add extra dependancies to this
+   little project
+*)
+
+let int = print_int;;
+
+let list pp_item l =
+  let rec print_content = function
+  | [] -> ()
+  | [x] -> pp_item x
+  | x::xs -> pp_item x;
+    Printf.printf "; ";
+    print_content xs in
+
+  Printf.printf "[";
+    print_content l;
+    Printf.printf "]"
